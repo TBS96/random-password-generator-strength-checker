a72c4328a9b72e5c2716aba96e9425ba3c6acd1b
TBS96 <9tbs6@proton.me>
2025-02-25 22:24:25 +0530

    implement checkStrength of password using if() and increment strength by 1.
    set an array of strengthTexts => Weak, Fair etc. and decrement 1 from strength to get the correct index from the array.
    
    changeBgColor according to pass. strength.
    
    TODO:
    
    1. instead of changing bgColor of whole document according to pass. strength, change the bgColor of strengthText instead.
    2. give border for password.
    3. create a btn to copy password using copy icon and tick icon beside password. when the btn is clicked, it should change the icon. (using toggle system)
    
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- js/script.js
+++ js/script.js
@@ -14,4 +14,45 @@ const generatePassword = passGenBtn.addEventListener('click', () => {
 
     const passwordShow = document.getElementById('password');
     passwordShow.textContent = password;
+
+    checkStrength(password);
 });
+
+const checkStrength = (password) => {
+    let strength = 0;
+
+    if (password.length >= 8) {
+        strength++;
+    }
+    if(/[A-Z]/.test(password)) {
+        strength++;
+    }
+    if(/[0-9]/.test(password)) {
+        strength++;
+    }
+    if(/[!@#$%^&*()]/.test(password)) {
+        strength++;
+    }
+    
+    const strengthText = ['Weak', 'Fair', 'Good', 'Strong'] [strength - 1] || 'Weak';
+    document.getElementById('strength').textContent = `Strength: ${strengthText}`;
+
+    changeBgColor(strength);
+
+    console.log(password, strength, strengthText);
+};
+
+const changeBgColor = (strength) => {
+    const bgColors = ['#ff4d4d', '#ffa500', '#4682b4', '#32cd32'];
+    document.body.style.backgroundColor = bgColors[strength - 1] || '#ffd4d4';
+};
+
+
+
+
+
+
+// TODO:
+// 1. instead of changing bgColor of whole document according to pass. strength, change the bgColor of strengthText instead.
+// 2. give border for password.
+// 3. create a btn to copy password using copy icon and tick icon beside password. when the btn is clicked, it should change the icon. (using toggle system)
